319e7104a91420325bb1af1af4d5b767539f9fe7
Lagunskij <lagunskij@gmail.com>
2019-10-29 19:59:27 +0300

    no message

--- src/by/academy/java/shchukin/classwork/lesson8/Class1.java
+++ src/by/academy/java/shchukin/classwork/lesson8/Class1.java
@@ -1,7 +1,5 @@
 package by.academy.java.shchukin.classwork.lesson8;
 
-import by.academy.java.shchukin.classwork.lesson8.Two.Version;
-
 @Version(versionNumber = "3.5.1")
 public class Class1 {
 
--- src/by/academy/java/shchukin/classwork/lesson8/Class2.java
+++ src/by/academy/java/shchukin/classwork/lesson8/Class2.java
@@ -1,7 +1,5 @@
 package by.academy.java.shchukin.classwork.lesson8;
 
-import by.academy.java.shchukin.classwork.lesson8.Two.Version;
-
 @Version(versionNumber = "3.5.2")
 public class Class2 {
 
