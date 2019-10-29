52a11dddaba382153e40f70e3234b2112d91b7ea
Lagunskij <lagunskij@gmail.com>
2019-10-29 21:52:46 +0300

    no message

---
+++ src/by/academy/java/shchukin/classwork/generics/Test.java
@@ -0,0 +1,12 @@
+package by.academy.java.shchukin.classwork.generics;
+
+public class Test {
+
+public static void main(String[] args) {
+Box<String> box = new Box<String>();
+box.setItem("Test");
+Object item = box.getItem();
+Integer itemInt = (Integer) item;
+}
+
+}
---
+++ src/by/academy/java/shchukin/classwork/lesson88/MethodExecutor.java
@@ -0,0 +1,25 @@
+package by.academy.java.shchukin.classwork.lesson88;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class MethodExecutor {
+
+public static void main(String[] args) throws IllegalAccessException,
+IllegalArgumentException, InvocationTargetException {
+executeAutoexecutableMethods(new Class1());
+executeAutoexecutableMethods(new Class2());
+}
+
+private static void executeAutoexecutableMethods(Object object)
+throws IllegalAccessException, IllegalArgumentException,
+InvocationTargetException {
+Class<? extends Object> clazz = object.getClass();
+
+Method[] methods = clazz.getMethods();
+for (Method method : methods) {
+if (method.isAnnotationPresent(Autoexecutable.class)) {
+method.invoke(object);
+}
+}
+}
+}
