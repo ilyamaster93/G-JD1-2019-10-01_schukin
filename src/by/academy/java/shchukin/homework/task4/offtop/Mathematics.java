a77227ee1ed1a709e87e46c3d4032738d9a92231
shchukin <ilyamaster93@gmail.com>
2019-11-14 20:52:13 +0300

    no message

---
+++ src/by/academy/java/shchukin/homework/task4/offtop/Mathematics.java
@@ -0,0 +1,22 @@
+package by.academy.java.shchukin.homework.task4.offtop;
+
+public class Mathematics {
+int a,b;
+
+public int getA() {
+	return a;
+}
+
+public void setA(int a) {
+	this.a = a;
+}
+
+public int getB() {
+	return b;
+}
+
+public void setB(int b) {
+	this.b = b;
+}
+
+}
